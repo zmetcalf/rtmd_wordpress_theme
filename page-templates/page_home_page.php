e380221131c9791567e4ce87ae2ac609ec7be755
Zachery Metcalf <zachery.metcalf@gmail.com>
2015-06-09 17:34:55 -0600

    Starting to add custom fields to home page template

--- page-templates/page_home_page.php
+++ page-templates/page_home_page.php
@@ -10,13 +10,11 @@
     <div class="row">
       <div class="large-12 large-offset-0 medium-10 medium-offset-1 small-10 small-offset-1 columns">
         <div class="home_slider">
+          <?php foreach ( get_post_meta( $post->ID, 'slider_image_url' ) as $image_url): ?>
           <div>
-            <img src="http://placehold.it/500x400&text=Image1">
-            <h5>Why hello, ask me what I need! Why hello, ask me what I need! Why hello, ask me what I need!</h5>
+            <img src="<?php echo $image_url; ?>">
           </div>
-          <div><img src="http://placehold.it/500x400&text=Image2"></div>
-          <div><img src="http://placehold.it/500x500&text=Image3"></div>
-          <div><img src="http://placehold.it/500x500&text=Image4"></div>
+          <?php endforeach; ?>
         </div>
       </div>
     </div>
@@ -29,23 +27,26 @@
     <h3 class="show-for-small">Header<hr/></h3>
 
     <div class="panel">
-      <h4>How we serve<hr/></h4>
-      <h5 class="subheader">Everyone deserves a safe and healthy home.  Rebuilding Together Metro Denver and its network of sponsors and volunteers provide crucial home repairs, safety modifications and energy efficiency upgrades to qualified homeowners and community centers in need.</h5>
+      <h4><?php echo get_post_meta( $post->ID, 'main_block_header', TRUE ); ?><hr/></h4>
+      <h5 class="subheader"><?php echo get_post_meta( $post->ID, 'main_block_body', TRUE); ?></h5>
     </div>
 
     <div class="row" data-equalizer="small_blocks">
       <div class="large-6 small-6 columns">
         <div class="panel" data-equalizer-watch="small_blocks">
-          <h5>Our Blog</h5>
-        <h6 class="subheader">Read more about what great things RTMD is doing in the community.</h6>
-        <a href="#" class="small button large-12">Start Reading</a>
+          <h5><?php echo get_post_meta( $post->ID, 'child_block_one_header', TRUE ); ?></h5>
+        <h6 class="subheader"><?php echo get_post_meta( $post->ID, 'child_block_one_body', TRUE ); ?></h6>
+        <?php for ($breaks = 0; $breaks < intval( get_post_meta( $post->ID, 'child_block_one_blank_space', TRUE ) ); $breaks++ ): ?>
+          <br>
+        <?php endfor; ?>
+        <a href="<?php echo get_post_meta( $post->ID, 'child_block_one_link', TRUE); ?>" class="small button large-12"><?php echo get_post_meta( $post->ID, 'child_block_one_button_text', TRUE); ?></a>
         </div>
       </div>
 
       <div class="large-6 small-6 columns">
         <div class="panel" data-equalizer-watch="small_blocks">
           <h5>Rebuilding Days</h5>
-          <h6 class="subheader">Find out more about the next rebuilding days.</h6><br>
+          <h6 class="subheader">Find out more about the next rebuilding days.</h6>
           <a href="#" class="small button large-12">Take a Look</a>
         </div>
       </div>
