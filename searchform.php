c34fbfcd6d63c5638b15f720fe6742eeb9056812
Zachery Metcalf <zachery.metcalf@gmail.com>
2015-06-11 18:08:07 -0600

    Added styling for sidebar lists

--- searchform.php
+++ searchform.php
@@ -1,10 +1,10 @@
 <!-- search -->
 <form class="search" method="get" action="<?php echo home_url(); ?>" role="search">
   <div class="row collapse">
-    <div class="large-8 columns">
+    <div class="large-8 small-8 columns">
       <input class="search-input expand" type="search" name="s" placeholder="<?php _e( 'Search', 'rtmd_theme' ); ?>">
     </div>
-    <div class="large-4 columns">
+    <div class="large-4 small-4 columns">
       <button class="search-submit button postfix expand" type="submit" role="button"><i class="fa fa-search"></i></button>
     </div>
   </div>
