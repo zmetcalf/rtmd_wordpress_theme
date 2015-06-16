e60e387ba43264fd6ad4472997bb430581d4c48e
Zachery Metcalf <zachery.metcalf@gmail.com>
2015-06-15 20:54:29 -0600

    Added in newsletter contact form

--- page-templates/page_home_page.php
+++ page-templates/page_home_page.php
@@ -58,20 +58,53 @@
 <div class="row">
   <div class="large-12 columns">
     <div class="radius panel">
-      <form>
+      <div id="success_message" style="display:none;" data-alert class="alert-box hidden radius">
+        Thanks for signing up!
+          <a href="#" class="close">&times;</a>
+      </div>
+
+      <form data-id="embedded_signup:form" class="ctct-custom-form Form" name="embedded_signup" method="POST" action="https://visitor2.constantcontact.com/api/signup">
+        <h5>Sign-up for our E-Newsletter</h5>
+        <!-- The following code must be included to ensure your sign-up form works properly. -->
+        <input data-id="ca:input" type="hidden" name="ca" value="238ed0d9-e683-4a81-ac8a-d503bcc2b4b6">
+        <input data-id="list:input" type="hidden" name="list" value="1561268378">
+        <input data-id="source:input" type="hidden" name="source" value="EFD">
+        <input data-id="required:input" type="hidden" name="required" value="email">
+        <input data-id="url:input" type="hidden" name="url" value="">
+
         <div class="row collapse">
 
-          <h5>Sign-up for our E-Newsletter</h5>
           <div class="large-10 small-8 columns">
-            <input type="text"/>
+            <p data-id="Email Address:p" >
+              <input data-id="Email Address:input" type="text" name="email" value="" maxlength="80">
+            </p>
           </div>
 
           <div class="large-2 small-4 columns">
-            <a href="#" class="postfix button expand">Subscribe</a>
+            <!--<a href="#" class="postfix button expand">Subscribe</a>-->
+            <button type="submit" class="postfix button expand Button ctct-button Button--block Button-secondary" data-enabled="enabled">Subscribe</button>
           </div>
 
         </div>
       </form>
+      <script type='text/javascript'>
+         var localizedErrMap = {};
+         localizedErrMap['required'] = 		'This field is required.';
+         localizedErrMap['ca'] = 			'An unexpected error occurred while attempting to send email.';
+         localizedErrMap['email'] = 			'Please enter your email address in name@email.com format.';
+         localizedErrMap['birthday'] = 		'Please enter birthday in MM/DD format.';
+         localizedErrMap['anniversary'] = 	'Please enter anniversary in MM/DD/YYYY format.';
+         localizedErrMap['custom_date'] = 	'Please enter this date in MM/DD/YYYY format.';
+         localizedErrMap['list'] = 			'Please select at least one email list.';
+         localizedErrMap['generic'] = 		'This field is invalid.';
+         localizedErrMap['shared'] = 		'Sorry, we could not complete your sign-up. Please contact us to resolve this.';
+         localizedErrMap['state_mismatch'] = 'Mismatched State/Province and Country.';
+         localizedErrMap['state_province'] = 'Select a state/province';
+         localizedErrMap['selectcountry'] = 	'Select a country';
+         var postURL = 'https://visitor2.constantcontact.com/api/signup';
+      </script>
+      <script type='text/javascript' src='https://static.ctctcdn.com/h/contacts-embedded-signup-assets/1.0.0/js/signup-form.js'></script>
+      <!--End CTCT Sign-Up Form-->
     </div>
   </div>
 </div>
