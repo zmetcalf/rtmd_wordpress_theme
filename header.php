cc4c7b997dc3123464a11f624db5490561e083fb
Zachery Metcalf <zachery.metcalf@gmail.com>
2015-11-17 19:44:32 -0700

    Removed meta description for yoast

--- header.php
+++ header.php
@@ -10,7 +10,6 @@
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo('description'); ?>">
 
 		<?php wp_head(); ?>
 
